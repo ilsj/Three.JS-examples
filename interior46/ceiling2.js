658467a6d1152ba953f696c88421dfdaafbe554e
ilsj <uralrek@mail.ru>
2017-01-18 02:06:58 +0500

    Added demo Interior 46, with wall color change options
    3DS Max Interior from FREE SAMPLE INTERIOR 46
    http://3darcshop.com/3dmodel/free-sample-interior-46.html

---
+++ interior46/ceiling2.js
@@ -0,0 +1,36 @@
+{
+
+"metadata":
+{
+"sourceFile": "3DARCSHOP.COM 3DSMAX 2012 06.max",
+"generatedBy": "3ds max ThreeJSExporter",
+"formatVersion": 3.1,
+"vertices": 4,
+"normals": 2,
+"colors": 0,
+"uvs": 4,
+"triangles": 2,
+"materials": 1
+},
+
+"materials": [
+{
+"DbgIndex" : 0,
+"DbgName"  : "dummy",
+"colorDiffuse"  : [1.0000, 0.0000, 0.0000],
+"vertexColors" : false
+}
+
+],
+
+"vertices": [2022.51,2700.0,-2056.46,-1977.49,2700.0,-2056.46,-1977.49,2700.0,2655.73,2022.51,2700.0,2655.73],
+
+"normals": [0.0,-1.0,0.0,0.0,-1.0,0.0],
+
+"colors": [],
+
+"uvs": [[0.02192,0.02192,0.97808,0.833568,0.02192,0.833568,0.97808,0.02192]],
+
+"faces": [42,2,0,3,0,0,1,2,0,0,0,42,1,0,2,0,3,1,0,1,1,1]
+
+}
---
+++ interior46/floor.js
@@ -0,0 +1,36 @@
+{
+
+"metadata":
+{
+"sourceFile": "3DARCSHOP.COM 3DSMAX 2012 06.max",
+"generatedBy": "3ds max ThreeJSExporter",
+"formatVersion": 3.1,
+"vertices": 4,
+"normals": 2,
+"colors": 0,
+"uvs": 4,
+"triangles": 2,
+"materials": 1
+},
+
+"materials": [
+{
+"DbgIndex" : 0,
+"DbgName"  : "dummy",
+"colorDiffuse"  : [1.0000, 0.0000, 0.0000],
+"vertexColors" : false
+}
+
+],
+
+"vertices": [2022.51,0.0,-4138.71,-1977.49,0.0,-4138.71,-1977.49,0.0,2730.86,2022.51,0.0,2730.86],
+
+"normals": [0.0,1.0,0.0,0.0,1.0,0.0],
+
+"colors": [],
+
+"uvs": [[0.97808,0.02192,0.97808,0.578671,0.02192,0.578671,0.02192,0.02192]],
+
+"faces": [42,2,3,0,0,0,1,2,0,0,0,42,1,2,0,0,3,0,2,1,1,1]
+
+}
